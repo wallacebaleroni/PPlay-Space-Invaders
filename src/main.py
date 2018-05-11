5c57ececd3bf31cbbabd2530fad3f4e253bb2893
Wallace Baleroni <wallacebaleroni@gmail.com>
2018-05-10 23:04:59 -0300

    merged with PPlay-Menu

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from PPlay.window import *
 from PPlay.gameimage import *
 from PPlay.sprite import *
 from PPlay.keyboard import *
+from PPlay.mouse import *
 
-WINDOW_SIZE_X = 500
-WINDOW_SIZE_Y = 500
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
+
+BUTTON_WIDTH = 250
+BUTTON_HEIGHT = 70
 
 SHOT_SIZE_X = 4
 SHOT_SIZE_Y = 14
@@ -17,21 +22,63 @@ BASE_SPEED = 100
 ENEMY_SIZE_X = 25
 ENEMY_SIZE_Y = 25
 
-ENEMY_START_X = 25
+ENEMY_START_X = 50
 ENEMY_START_Y = 25
 
 ENEMY_SPACING_X = 40
 ENEMY_SPACING_Y = 40
 
+BUTTON_BORDER = 30
+BUTTON_SPACING = 20
+
 
 def main():
-    window = Window(WINDOW_SIZE_X, WINDOW_SIZE_Y)
-    window.set_title("Space Invaders")
+    game_window = Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-    background = GameImage("img/background.png")
+    mouse_input = Mouse()
 
-    spaceship = Sprite("img/spaceship.png")
-    spaceship.set_position(50, WINDOW_SIZE_Y - 50)
+    background = GameImage("../img/camaro-2017-1.jpg")
+
+    bt_play = Sprite("../img/jogar.png", 2)
+    bt_difficulty = Sprite("../img/dificuldade.png", 1)
+    bt_ranking = Sprite("../img/ranking.png", 1)
+    bt_exit = Sprite("../img/sair.png", 1)
+    bt_play.set_position(WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2, BUTTON_BORDER)
+    bt_difficulty.set_position(WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2, BUTTON_BORDER + (BUTTON_HEIGHT + BUTTON_SPACING))
+    bt_ranking.set_position(WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2, BUTTON_BORDER + (BUTTON_HEIGHT + BUTTON_SPACING) * 2)
+    bt_exit.set_position(WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2, BUTTON_BORDER + (BUTTON_HEIGHT + BUTTON_SPACING) * 3)
+
+    while True:
+        if mouse_input.is_over_object(bt_play):
+            bt_play.set_curr_frame(1)
+            if mouse_input.is_button_pressed(1):
+                game()
+        else:
+            bt_play.set_curr_frame(0)
+        if mouse_input.is_over_object(bt_difficulty) and mouse_input.is_button_pressed(1):
+            menu_difficulty()
+        if mouse_input.is_over_object(bt_ranking) and mouse_input.is_button_pressed(1):
+            print("Ranking")
+        if mouse_input.is_over_object(bt_exit) and mouse_input.is_button_pressed(1):
+            return
+
+        background.draw()
+        bt_play.draw()
+        bt_difficulty.draw()
+        bt_ranking.draw()
+        bt_exit.draw()
+
+        game_window.update()
+
+
+def game():
+    p_window = Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+    p_window.set_title("Space Invaders")
+
+    background = GameImage("../img/background.png")
+
+    spaceship = Sprite("../img/spaceship.png")
+    spaceship.set_position(50, WINDOW_HEIGHT - 50)
 
     spaceship_speed = 2 * BASE_SPEED
 
@@ -43,8 +90,9 @@ def main():
     for i in range(2):
         line = []
         for j in range(5):
-            enemy = Sprite("img/enemy.png")
-            enemy.set_position(ENEMY_START_X + j * (ENEMY_SIZE_X + ENEMY_SPACING_X),ENEMY_START_Y + i * (ENEMY_SIZE_Y + ENEMY_SPACING_Y))
+            enemy = Sprite("../img/enemy.png")
+            enemy.set_position(ENEMY_START_X + j * (ENEMY_SIZE_X + ENEMY_SPACING_X),
+                               ENEMY_START_Y + i * (ENEMY_SIZE_Y + ENEMY_SPACING_Y))
             line.append(enemy)
         enemies.append(line)
     enemy_speed_x = 0.3 * BASE_SPEED
@@ -57,23 +105,23 @@ def main():
     kb = Keyboard()
 
     total_time = 0
-    last_shot_time = window.delta_time()
-    last_enemy_move = window.delta_time()
+    last_shot_time = p_window.delta_time()
+    last_enemy_move = p_window.delta_time()
 
     while True:
-        time = window.delta_time()
-        total_time += time
+        delta_time = p_window.delta_time()
+        total_time += delta_time
 
         # spaceship move
         if kb.key_pressed("RIGHT"):
-            if spaceship.x + SPACESHIP_SIZE_X <= WINDOW_SIZE_X:
-                spaceship.set_position(spaceship.x + spaceship_speed * time, spaceship.y)
+            if spaceship.x + SPACESHIP_SIZE_X <= WINDOW_WIDTH:
+                spaceship.set_position(spaceship.x + spaceship_speed * delta_time, spaceship.y)
         elif kb.key_pressed("LEFT"):
             if spaceship.x >= 0:
-                spaceship.set_position(spaceship.x - spaceship_speed * time, spaceship.y)
+                spaceship.set_position(spaceship.x - spaceship_speed * delta_time, spaceship.y)
         if kb.key_pressed("SPACE"):
             if total_time - last_shot_time >= 0.1:
-                new_shot = Sprite("img/shot.png")
+                new_shot = Sprite("../img/shot.png")
                 new_shot.set_position(spaceship.x + SPACESHIP_SIZE_X / 2, spaceship.y - SHOT_SIZE_Y)
                 shots.append(new_shot)
                 last_shot_time = total_time
@@ -82,13 +130,13 @@ def main():
 
         # update shots positions
         for shot in shots:
-            shot.set_position(shot.x, shot.y - shot_speed * time)
+            shot.set_position(shot.x, shot.y - shot_speed * delta_time)
             if shot.y <= 0 - SHOT_SIZE_Y:
                 shots.remove(shot)
 
         # update enemies positions
         if total_time - last_enemy_move > 1:
-            if enemy_n_of_moves_x > 4:
+            if enemy_n_of_moves_x > 5:
                 enemy_n_of_moves_x = 0
                 enemy_move_x = 0
                 enemy_move_y = 1
@@ -115,6 +163,37 @@ def main():
             for enemy in line:
                 if enemy is not None:
                     enemy.draw()
-        window.update()
+        p_window.update()
+
+
+def menu_difficulty():
+    game_window = Window(600, 400)
+
+    mouse_input = Mouse()
+
+    background = GameImage("../img/camaro-2017-1.jpg")
+
+    bt_easy = Sprite("../img/facil.png", 1)
+    bt_medium = Sprite("../img/medio.png", 1)
+    bt_hard = Sprite("../img/dificil.png", 1)
+    bt_easy.set_position(175, 63)
+    bt_medium.set_position(175, 156)
+    bt_hard.set_position(175, 250)
+
+    while True:
+        if mouse_input.is_over_object(bt_easy) and mouse_input.is_button_pressed(1):
+            print("Easy")
+        if mouse_input.is_over_object(bt_medium) and mouse_input.is_button_pressed(1):
+            print("Medium")
+        if mouse_input.is_over_object(bt_hard) and mouse_input.is_button_pressed(1):
+            print("Hard")
+
+        background.draw()
+        bt_easy.draw()
+        bt_medium.draw()
+        bt_hard.draw()
+
+        game_window.update()
+
 
 main()
